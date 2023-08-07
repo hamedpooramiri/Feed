014290d6aab90e4d5ded90dba09d1af8532473a5
hamed <hamedpooramiri1007@gmail.com>
2023-08-07 04:39:46 +0330

    rename RemoteFeedLoaderTest.swift

--- FeedTests/RemoteFeedLoaderTest.swift
+++ FeedTests/RemoteFeedLoaderTest.swift
@@ -7,7 +7,7 @@
 
 import XCTest
 
-final class RemoteFeedLoader: XCTestCase {
+final class RemoteFeedLoaderTest: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
