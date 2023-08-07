7f56c2a444219dc9bb1db0bd4d5bdd3a2ec35d9a
hamed <hamedpooramiri1007@gmail.com>
2023-08-07 03:30:03 +0330

    init git repository

---
+++ Feed/Feed.h
@@ -0,0 +1,18 @@
+//
+//  Feed.h
+//  Feed
+//
+//  Created by hamedpouramiri on 8/7/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Feed.
+FOUNDATION_EXPORT double FeedVersionNumber;
+
+//! Project version string for Feed.
+FOUNDATION_EXPORT const unsigned char FeedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Feed/PublicHeader.h>
+
+
