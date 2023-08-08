a0a1960d76d5b7b1e5ffdde3faba44fb018bd3e5
hamed <hamedpooramiri1007@gmail.com>
2023-08-09 02:59:53 +0330

    complete implementing FeedAPIEndToEndTests

--- FeedAPIEndToEndTests/FeedAPIEndToEndTests.swift
+++ FeedAPIEndToEndTests/FeedAPIEndToEndTests.swift
@@ -6,15 +6,32 @@
 //
 
 import XCTest
+import Feed
 
 final class FeedAPIEndToEndTests: XCTestCase {
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    func test_endToEndTest_ServerGetFeedResult_matchesFixedTestAccountData() throws {
+        let url = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
+        let session = URLSession.shared
+        let client = URLSessionHTTPClient(session: session)
+        let loader = RemoteFeedLoader(url: url, client: client)
+        let exp = expectation(description: "waiting for load data")
+
+        var expectedResult: LoadFeedResult?
+        loader.load { result in
+            expectedResult = result
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 10.0)
+        
+        switch expectedResult {
+        case .success(let items)?:
+            XCTAssertEqual(items.count, 8, "expected 8 items to receive from test account")
+        case .failure(let error)?:
+            XCTFail("expected to get successful result but got error \(error)")
+        default:
+            XCTFail("expected to get successful result but got no result")
+        }
     }
 
 }
