c2409bbcc7dcdbcc20228c2d2d37ffa6246bc590
hamed <hamedpooramiri1007@gmail.com>
2023-08-09 03:21:32 +0330

    add new Cases for Checking the incoming data from the server in  FeedAPIEndToEndTests

--- FeedAPIEndToEndTests/FeedAPIEndToEndTests.swift
+++ FeedAPIEndToEndTests/FeedAPIEndToEndTests.swift
@@ -27,6 +27,8 @@ final class FeedAPIEndToEndTests: XCTestCase {
         switch expectedResult {
         case .success(let items)?:
             XCTAssertEqual(items.count, 8, "expected 8 items to receive from test account")
+            XCTAssertEqual(items[0].id.uuidString, "73A7F70C-75DA-4C2E-B5A3-EED40DC53AA6")
+            XCTAssertEqual(items[0].location, "Location 1")
         case .failure(let error)?:
             XCTFail("expected to get successful result but got error \(error)")
         default:
@@ -36,4 +38,55 @@ final class FeedAPIEndToEndTests: XCTestCase {
         trackForMemoryLeaks(client)
     }
 
+    // MARK: The fixed Data Expected From the server
+    /*
+     {
+         "items": [
+             {
+                 "id": "73A7F70C-75DA-4C2E-B5A3-EED40DC53AA6",
+                 "description": "Description 1",
+                 "location": "Location 1",
+                 "image": "https://url-1.com",
+             },
+             {
+                 "id": "BA298A85-6275-48D3-8315-9C8F7C1CD109",
+                 "location": "Location 2",
+                 "image": "https://url-2.com",
+             },
+             {
+                 "id": "5A0D45B3-8E26-4385-8C5D-213E160A5E3C",
+                 "description": "Description 3",
+                 "image": "https://url-3.com",
+             },
+             {
+                 "id": "FF0ECFE2-2879-403F-8DBE-A83B4010B340",
+                 "image": "https://url-4.com",
+             },
+             {
+                 "id": "DC97EF5E-2CC9-4905-A8AD-3C351C311001",
+                 "description": "Description 5",
+                 "location": "Location 5",
+                 "image": "https://url-5.com",
+             },
+             {
+                 "id": "557D87F1-25D3-4D77-82E9-364B2ED9CB30",
+                 "description": "Description 6",
+                 "location": "Location 6",
+                 "image": "https://url-6.com",
+             },
+             {
+                 "id": "A83284EF-C2DF-415D-AB73-2A9B8B04950B",
+                 "description": "Description 7",
+                 "location": "Location 7",
+                 "image": "https://url-7.com",
+             },
+             {
+                 "id": "F79BD7F8-063F-46E2-8147-A67635C3BB01",
+                 "description": "Description 8",
+                 "location": "Location 8",
+                 "image": "https://url-8.com",
+             }
+         ]
+     }
+     */
 }
