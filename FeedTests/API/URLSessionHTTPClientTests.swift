76a586494ef607601a71a479110864621f99d1bf
hamed <hamedpooramiri1007@gmail.com>
2023-08-08 18:34:06 +0330

    refactor(URLSessionHTTPClientTests): add method for track memory Leaks on SUT

--- FeedTests/API/URLSessionHTTPClientTests.swift
+++ FeedTests/API/URLSessionHTTPClientTests.swift
@@ -30,9 +30,17 @@ class URLSessionHTTPClient {
 
 final class URLSessionHTTPClientTests: XCTestCase {
     
+    override class func setUp() {
+        super.setUp()
+        URLProtocolStub.startInterceptingRequests()
+    }
+    
+    override class func tearDown() {
+        super.tearDown()
+        URLProtocolStub.stopInterceptingRequests()
+    }
     
     func test_getFromURL_performGetRequest() {
-        URLProtocolStub.startInterceptingRequests()
         let url = URL(string: "http://a-url.com")!
         let exp = expectation(description: "wait for request")
         URLProtocolStub.observeRequests { request in
@@ -40,21 +48,17 @@ final class URLSessionHTTPClientTests: XCTestCase {
             XCTAssertEqual(request.httpMethod, "GET")
             exp.fulfill()
         }
-        
         makeSUT().get(from: url) { _ in}
         wait(for: [exp])
-        URLProtocolStub.stopInterceptingRequests()
     }
     
     func test_getFromURL_failsOnRequestError() {
-        URLProtocolStub.startInterceptingRequests()
         let url = URL(string: "http://a-url.com")!
         let domainError = NSError(domain: "a error", code: 1)
         URLProtocolStub.stub(data: nil, response: nil, error: domainError)
 
-        let sut = makeSUT()
         let exp = expectation(description: "a wait")
-        sut.get(from: url) { result in
+        makeSUT().get(from: url) { result in
             if case .failure(let error as NSError) = result {
                 XCTAssertEqual(error.code, domainError.code)
                 XCTAssertEqual(error.localizedDescription, domainError.localizedDescription)
@@ -64,18 +68,25 @@ final class URLSessionHTTPClientTests: XCTestCase {
             exp.fulfill()
         }
         wait(for: [exp], timeout: 1)
-        URLProtocolStub.stopInterceptingRequests()
     }
     
     //MARK: - Helper
     
-    func makeSUT() -> URLSessionHTTPClient {
+    func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> URLSessionHTTPClient {
         let config = URLSessionConfiguration.default
         config.protocolClasses?.insert(URLProtocolStub.self, at: 0)
         let session = URLSession(configuration: config)
-        return URLSessionHTTPClient(session: session)
+        let sut = URLSessionHTTPClient(session: session)
+        trackForMemoryLeaks(sut)
+        return sut
     }
     
+    func trackForMemoryLeaks(_ instance: AnyObject, file: StaticString = #filePath, line: UInt = #line) {
+        addTeardownBlock { [weak instance] in
+            XCTAssertNil(instance, "Instance should have been deallocated. potential memory leak.", file: file, line: line)
+        }
+    }
+
     private class URLProtocolStub: URLProtocol {
 
         private static var stub: Stub?
