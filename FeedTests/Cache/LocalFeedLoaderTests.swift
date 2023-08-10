695978985bf1bbea2314b3b2618810ecf78b9637
hamed <hamedpooramiri1007@gmail.com>
2023-08-10 07:12:55 +0330

    feature(LocalFeedLoaderTests): implement makeSUT factory Methode

--- FeedTests/Cache/LocalFeedLoaderTests.swift
+++ FeedTests/Cache/LocalFeedLoaderTests.swift
@@ -6,30 +6,61 @@
 //
 
 import XCTest
+import Feed
+
+class FeedStore {
+    var deleteCacheCount: Int = 0
+    var savedSuccessCount: Int = 0
+   
+    func deleteCachedFeeds() {
+        deleteCacheCount += 1
+    }
+}
+
+class LocalFeedLoader {
+
+    private var store: FeedStore
+    init(store: FeedStore) {
+        self.store = store
+    }
+    
+    func save(items: [FeedItem]){
+        store.deleteCachedFeeds()
+    }
+}
+
 
 final class LocalFeedLoaderTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func test_notDeleteTheCacheOnCreation() {
+        let (store, _) = makeSUT()
+        XCTAssertEqual(store.deleteCacheCount, 0)
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func test_saveFeeds_deletePreviousCachesItems() {
+        let (store, sut) = makeSUT()
+        sut.save(items: [uniqueFeedItem(), uniqueFeedItem()])
+        XCTAssertEqual(store.deleteCacheCount, 1)
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    func makeSUT() -> (store: FeedStore, sut: LocalFeedLoader){
+        let store = FeedStore()
+        let sut = LocalFeedLoader(store: store)
+        trackForMemoryLeaks(sut)
+        trackForMemoryLeaks(store)
+        return (store, sut)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    //MARK: - Helpers
+    func uniqueFeedItem() -> FeedItem {
+        FeedItem(id: UUID(), description: "any description", location: "any location", imageUrl: anyURL())
     }
-
+    
+    func anyURL() -> URL {
+        URL(string: "http://any-url.com")!
+    }
+    
+    func anyNSError() -> NSError {
+        NSError(domain: "any error", code: 0)
+    }
 }
