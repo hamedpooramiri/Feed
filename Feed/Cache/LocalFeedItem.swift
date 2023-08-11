39d2b47dc02017e26943add54ed356ea35f8f6a2
hamed <hamedpooramiri1007@gmail.com>
2023-08-11 05:28:51 +0330

    complete implementing LocalFeedItem

--- Feed/Cache/LocalFeedItem.swift
+++ Feed/Cache/LocalFeedItem.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+
+public struct LocalFeedItem: Equatable {
+    public let id: UUID
+    public let description: String?
+    public let location: String?
+    public let imageUrl: URL
+    
+    public init(id: UUID, description: String?, location: String?, imageUrl: URL) {
+        self.id = id
+        self.description = description
+        self.location = location
+        self.imageUrl = imageUrl
+    }
+
+}
+
+extension Array where Element == FeedItem {
+    func toLocal() -> [LocalFeedItem] {
+        map { LocalFeedItem(id: $0.id, description: $0.description, location: $0.location, imageUrl: $0.imageUrl) }
+    }
+}
