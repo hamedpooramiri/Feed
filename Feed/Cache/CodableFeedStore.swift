9e9392645bb6fa0c8616438a5d4eb45f6498e641
hamed <hamedpooramiri1007@gmail.com>
2023-08-12 20:15:25 +0330

    implement first version of CodableFeedStoreTests

--- Feed/Cache/CodableFeedStore.swift
+++ Feed/Cache/CodableFeedStore.swift
@@ -6,3 +6,53 @@
 //
 
 import Foundation
+
+public class CodableFeedStore {
+
+    let storeURL: URL
+
+    struct Cache: Codable {
+        var feedItems: [CodableFeedItem]
+        var timeStamp: Date
+    }
+
+    struct CodableFeedItem: Codable {
+        public let id: UUID
+        public let description: String?
+        public let location: String?
+        public let imageUrl: URL
+        
+        init(localFeed: LocalFeedItem) {
+            id = localFeed.id
+            description = localFeed.description
+            location = localFeed.location
+            imageUrl = localFeed.imageUrl
+        }
+        
+        public var localFeed: LocalFeedItem {
+            .init(id: id, description: description, location: location, imageUrl: imageUrl)
+        }
+    }
+    
+    public init(storeURL: URL) {
+        self.storeURL = storeURL
+    }
+
+    public func insert(feeds: [LocalFeedItem], timeStamp: Date, completion: @escaping FeedStore.InsertCompletion) {
+        let codablefeeds = feeds.map(CodableFeedItem.init)
+        let jsonEncoded = try! JSONEncoder().encode(Cache(feedItems: codablefeeds, timeStamp: timeStamp))
+        try! jsonEncoded.write(to: storeURL)
+        completion(nil)
+    }
+
+   public func retrieve(completion: @escaping FeedStore.retrieveCompletion) {
+       do {
+           let data = try Data(contentsOf: storeURL)
+           let cache = try JSONDecoder().decode(Cache.self, from: data)
+           completion(.found(items: cache.feedItems.map(\.localFeed), timeStamp: cache.timeStamp))
+       } catch {
+           completion(.empty)
+       }
+   }
+
+}
--- FeedTests/Cache/CodableFeedStoreTests.swift
+++ FeedTests/Cache/CodableFeedStoreTests.swift
@@ -6,30 +6,98 @@
 //
 
 import XCTest
+import Feed
 
 final class CodableFeedStoreTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    override func setUp() {
+        super.setUp()
+        setUpEmptyStoreState()
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    override func tearDown() {
+        super.tearDown()
+        undoStoreSideEffects()
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    func test_retrieve_noCachesItems_returnEmpty() {
+        let sut = makeSUT()
+        expect(sut, toCompleteRetrieveWith: .empty)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    func test_retrieve_returnItemsSuccessfuly() {
+        let sut = makeSUT()
+        let timeStamp = Date()
+        let items = uniqueFeeds()
+        let exp = expectation(description: "wait to retrieve items")
+        sut.insert(feeds: items.localItems, timeStamp: timeStamp) { error in
+            XCTAssertNil(error)
+            exp.fulfill()
         }
+        wait(for: [exp], timeout: 1)
+        expect(sut, toCompleteRetrieveWith: .found(items: items.localItems, timeStamp: timeStamp))
     }
 
+    func test_retrieve_callTwice_hasNoSideEffect() {
+        let sut = makeSUT()
+        let timeStamp = Date()
+        let items = uniqueFeeds()
+        let exp = expectation(description: "wait to retrieve items")
+        sut.insert(feeds: items.localItems, timeStamp: timeStamp) { error in
+            XCTAssertNil(error)
+            sut.retrieve { firstResult in
+                sut.retrieve { secendResult in
+                    switch (firstResult, secendResult) {
+                    case let (.found(firstRetrivedItems, firstRetrievedTimeStamp), .found(secendRetrivedItems, secendRetrievedTimeStamp)):
+                        XCTAssertEqual(firstRetrivedItems, secendRetrivedItems)
+                        XCTAssertEqual(firstRetrievedTimeStamp, secendRetrievedTimeStamp)
+                    default:
+                        XCTFail("expected to get the same items but firstResult: \(firstResult) , and secendResult: \(secendResult)")
+                    }
+                    exp.fulfill()
+                }
+            }
+        }
+        wait(for: [exp], timeout: 1)
+    }
+    
+    // MARK: Helper
+    
+    func makeSUT() -> CodableFeedStore {
+        CodableFeedStore(storeURL: storeURLForTest())
+    }
+
+    func expect(_ sut: CodableFeedStore, toCompleteRetrieveWith expectedResult: FeedStoreRetrieveResult, file: StaticString = #filePath, line: UInt = #line) {
+        let exp = expectation(description: "wait for retrieve")
+        sut.retrieve { receivedresult in
+            switch (receivedresult, expectedResult) {
+            case let (.found(receivedItems, receivedTimeStamp), .found(expectedItems, expectedTimeStamp)):
+                XCTAssertEqual(receivedItems, expectedItems, file: file, line: line)
+                XCTAssertEqual(receivedTimeStamp, expectedTimeStamp, file: file, line: line)
+            case let (.failure(recievedError), .failure(expectedError)):
+                XCTAssertEqual(recievedError as NSError, expectedError as NSError)
+            case (.empty, .empty):
+                break
+            default:
+                XCTFail("expected to get \(expectedResult) but got \(receivedresult)", file: file, line: line)
+            }
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 1)
+    }
+    func setUpEmptyStoreState() {
+        deleteStoreArtifacts()
+    }
+
+    func undoStoreSideEffects() {
+        deleteStoreArtifacts()
+    }
+
+    func deleteStoreArtifacts() {
+        try? FileManager.default.removeItem(at: storeURLForTest())
+    }
+
+    func storeURLForTest() -> URL {
+        FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!.appending(path: "\(type(of: self)).store")
+    }
 }
