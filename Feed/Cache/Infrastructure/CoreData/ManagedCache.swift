1383bb5f1826f935b1d5d7958569de6ee6959af8
hamed <hamedpooramiri1007@gmail.com>
2023-08-14 03:30:14 +0330

    complete implementing ManagedFeedItem and  ManagedCache

--- Feed/Cache/Infrastructure/CoreData/ManagedCache.swift
+++ Feed/Cache/Infrastructure/CoreData/ManagedCache.swift
@@ -5,4 +5,32 @@
 //  Created by hamedpouramiri on 8/14/23.
 //
 
-import Foundation
+import CoreData
+
+
+@objc(ManagedCache)
+class ManagedCache: NSManagedObject {
+    @NSManaged var timestamp: Date
+    @NSManaged var feed: NSOrderedSet
+}
+
+extension ManagedCache {
+    static func find(in context: NSManagedObjectContext) throws -> ManagedCache? {
+        let request = NSFetchRequest<ManagedCache>(entityName: entity().name!)
+        request.returnsObjectsAsFaults = false
+        return try context.fetch(request).first
+    }
+    
+    static func deleteCache(in context: NSManagedObjectContext) throws {
+        try find(in: context).map(context.delete).map(context.save)
+    }
+    
+    static func newUniqueInstance(in context: NSManagedObjectContext) throws -> ManagedCache {
+        try deleteCache(in: context)
+        return ManagedCache(context: context)
+    }
+    
+    var localFeed: [LocalFeedItem] {
+        return feed.compactMap { ($0 as? ManagedFeedItem)?.local }
+    }
+}
--- Feed/Cache/Infrastructure/CoreData/ManagedFeedItem.swift
+++ Feed/Cache/Infrastructure/CoreData/ManagedFeedItem.swift
@@ -5,4 +5,19 @@
 //  Created by hamedpouramiri on 8/14/23.
 //
 
-import Foundation
+import CoreData
+
+@objc(ManagedFeedItem)
+class ManagedFeedItem: NSManagedObject {
+    @NSManaged var id: UUID
+    @NSManaged var itemDescription: String?
+    @NSManaged var location: String?
+    @NSManaged var url: URL
+    @NSManaged var cache: ManagedCache
+}
+
+extension ManagedFeedItem {
+    var local: LocalFeedItem {
+        return LocalFeedItem(id: id, description: itemDescription, location: location, imageUrl: url)
+        }
+}
