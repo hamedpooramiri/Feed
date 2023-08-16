cedc54a7778f294e6e81a6b3225c00875ae01ca1
hamed <hamedpooramiri1007@gmail.com>
2023-08-16 17:56:25 +0330

    add file FeedViewControllerTests

--- FeediOSTests/FeedViewControllerTests.swift
+++ FeediOSTests/FeedViewControllerTests.swift
@@ -1,14 +1,13 @@
 //
-//  FeediOSTests.swift
+//  FeedViewControllerTests.swift
 //  FeediOSTests
 //
 //  Created by hamedpouramiri on 8/16/23.
 //
 
 import XCTest
-@testable import FeediOS
 
-final class FeediOSTests: XCTestCase {
+final class FeedViewControllerTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
