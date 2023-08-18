7325615bc878bb1b3416f36feb3c670067f468ca
hamed <hamedpooramiri1007@gmail.com>
2023-08-18 23:13:48 +0330

    move FeedUIComposer code into its own file

--- FeediOS/Feed UI/Composers/FeedUIComposer.swift
+++ FeediOS/Feed UI/Composers/FeedUIComposer.swift
@@ -5,4 +5,21 @@
 //  Created by hamedpouramiri on 8/18/23.
 //
 
-import Foundation
+import UIKit
+import Feed
+
+public final class FeedUIComposer {
+    private init() {}
+    public static func makeFeed(loader: FeedLoader, imageLoader: FeedImageLoader) -> FeedViewController {
+        let refreshController = FeedRefreshViewController(feedLoader: loader)
+        let feedViewController = FeedViewController(refreshController: refreshController)
+        refreshController.onRefresh = adaptFeedToCellControllers(forwardingTo: feedViewController, imageLoader: imageLoader)
+        return feedViewController
+    }
+    // adapter pattern that convert => [feedItem] -To- [FeedCellViewController]
+    private static func adaptFeedToCellControllers(forwardingTo controller: FeedViewController, imageLoader: FeedImageLoader) -> (([FeedItem]) -> Void){
+        return { [weak controller] feed in
+            controller?.feed = feed.map { FeedCellViewController(imageLoader: imageLoader, model: $0) }
+        }
+    }
+}
--- FeediOS/Feed UI/Controllers/FeedViewController.swift
+++ FeediOS/Feed UI/Controllers/FeedViewController.swift
@@ -8,22 +8,6 @@
 import UIKit
 import Feed
 
-public final class FeedUIComposer {
-    private init() {}
-    public static func makeFeed(loader: FeedLoader, imageLoader: FeedImageLoader) -> FeedViewController {
-        let refreshController = FeedRefreshViewController(feedLoader: loader)
-        let feedViewController = FeedViewController(refreshController: refreshController)
-        refreshController.onRefresh = adaptFeedToCellControllers(forwardingTo: feedViewController, imageLoader: imageLoader)
-        return feedViewController
-    }
-    // adapter pattern that convert => [feedItem] -To- [FeedCellViewController]
-    private static func adaptFeedToCellControllers(forwardingTo controller: FeedViewController, imageLoader: FeedImageLoader) -> (([FeedItem]) -> Void){
-        return { [weak controller] feed in
-            controller?.feed = feed.map { FeedCellViewController(imageLoader: imageLoader, model: $0) }
-        }
-    }
-}
-
 public class FeedViewController: UITableViewController, UITableViewDataSourcePrefetching {
     
     private var refreshController: FeedRefreshViewController?
