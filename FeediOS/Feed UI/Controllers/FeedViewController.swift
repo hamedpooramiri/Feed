7ec9c92c380aefb3733380063237dd8ae706eba9
hamed <hamedpooramiri1007@gmail.com>
2023-08-18 22:28:35 +0330

    move FeedCellViewController codes into own file FeedCellViewController

--- FeediOS/Feed UI/Controllers/FeedViewController.swift
+++ FeediOS/Feed UI/Controllers/FeedViewController.swift
@@ -8,55 +8,6 @@
 import UIKit
 import Feed
 
-final class FeedCellViewController {
-
-    let imageLoader: FeedImageLoader?
-    let model: FeedItem
-    var task: ImageLoaderTask?
-    
-    init(imageLoader: FeedImageLoader?, model: FeedItem) {
-        self.imageLoader = imageLoader
-        self.model = model
-    }
-
-    func view() -> UITableViewCell {
-        let cell = FeedItemCell()
-        cell.locationContainer.isHidden = (model.location == nil)
-        cell.locationLabel.text = model.location
-        cell.descriptionLabel.text = model.description
-        cell.imageContainer.isShimmering = true
-        cell.retryButton.isHidden = true
-        cell.feedImageView.image = nil
-        let loadImage = { [weak self, weak cell] in
-            guard let self = self else { return }
-            self.task = self.imageLoader?.loadImage(with: model.imageUrl) { [weak cell] result in
-                switch result {
-                case .failure:
-                    cell?.retryButton.isHidden = false
-                case .success(let imageData):
-                    if let image = UIImage(data: imageData) {
-                        cell?.feedImageView.image = image
-                    } else {
-                        cell?.retryButton.isHidden = false
-                    }
-                }
-                cell?.imageContainer.isShimmering = false
-            }
-        }
-        cell.onRetry = loadImage
-        loadImage()
-        return cell
-    }
-
-    func preload() {
-        task = imageLoader?.loadImage(with: model.imageUrl, completion: { _ in })
-    }
-
-    deinit {
-        task?.cancel()
-    }
-}
-
 public class FeedViewController: UITableViewController, UITableViewDataSourcePrefetching {
     
     private var refreshController: FeedRefreshViewController?
