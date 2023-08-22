281613ca2d09e87336480e059b1b51e584ea6837
hamed <hamedpooramiri1007@gmail.com>
2023-08-22 13:39:40 +0330

    move  ImageLoaderPresentationAdapter code to its own file

--- FeediOS/Feed UI/Composers/Adapter/ImageLoaderPresentationAdapter.swift
+++ FeediOS/Feed UI/Composers/Adapter/ImageLoaderPresentationAdapter.swift
@@ -6,3 +6,39 @@
 //
 
 import Foundation
+import Feed
+
+final class ImageLoaderPresentationAdapter: FeedCellViewControllerDelegate {
+
+    private let imageLoader: FeedImageLoader
+    var presenter: FeedCellPresenterInput?
+    private let model: FeedItem
+    private var task: ImageLoaderTask?
+
+    init(imageLoader: FeedImageLoader, model: FeedItem) {
+        self.imageLoader = imageLoader
+        self.model = model
+    }
+
+    func didRequestLoadImage() {
+        presenter?.didStartLoadingImage(for: model)
+        let model = self.model
+        task = imageLoader.loadImage(with: model.imageUrl) { [weak self, model] result in
+            switch result {
+            case let .failure(error):
+                self?.presenter?.didFinishedLoadingImage(for: model, with: error)
+            case .success(let imageData):
+                self?.presenter?.didFinishedLoadingImage(for: model, with: imageData)
+            }
+        }
+    }
+    
+    func didRequestPreLoad() {
+        task = imageLoader.loadImage(with: model.imageUrl, completion: { _ in })
+    }
+    
+    func didRequestCancelLoad() {
+        task?.cancel()
+    }
+
+}
