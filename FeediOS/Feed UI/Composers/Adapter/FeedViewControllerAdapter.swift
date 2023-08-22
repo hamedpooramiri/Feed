04c38e2c83ce7b5179e487ae4ffeb67deb29b4c0
hamed <hamedpooramiri1007@gmail.com>
2023-08-22 13:41:53 +0330

    move FeedViewControllerAdapter code to tis own file

--- FeediOS/Feed UI/Composers/Adapter/FeedViewControllerAdapter.swift
+++ FeediOS/Feed UI/Composers/Adapter/FeedViewControllerAdapter.swift
@@ -5,4 +5,27 @@
 //  Created by hamedpouramiri on 8/22/23.
 //
 
-import Foundation
+import UIKit
+import Feed
+
+// adapter pattern that convert => [feedItem] -To- [FeedCellViewController]
+final class FeedViewControllerAdapter: FeedView {
+    // make refrence to Controller Weak  to prevent the memory leak
+    private weak var controller: FeedViewController?
+    private let imageLoader: FeedImageLoader
+    
+    init(controller: FeedViewController?, imageLoader: FeedImageLoader) {
+        self.controller = controller
+        self.imageLoader = imageLoader
+    }
+    
+    func display(_ viewModel: FeedViewModel) {
+        controller?.feed = viewModel.feed.map {
+            let imageLoaderPresentationAdapter = ImageLoaderPresentationAdapter(imageLoader: imageLoader, model: $0)
+            let controller = FeedCellViewController(delegate: imageLoaderPresentationAdapter)
+            let presenter = FeedCellPresenter<WeakRefVirtualProxy<FeedCellViewController>, UIImage>(feedCellView: WeakRefVirtualProxy(controller), imageTransformer: UIImage.init)
+            imageLoaderPresentationAdapter.presenter = presenter
+            return controller
+        }
+    }
+}
--- FeediOS/Feed UI/Composers/FeedUIComposer.swift
+++ FeediOS/Feed UI/Composers/FeedUIComposer.swift
@@ -23,25 +23,3 @@ public final class FeedUIComposer {
         return feedViewController
     }
 }
-
-// adapter pattern that convert => [feedItem] -To- [FeedCellViewController]
-final class FeedViewControllerAdapter: FeedView {
-    // make refrence to Controller Weak  to prevent the memory leak
-    private weak var controller: FeedViewController?
-    private let imageLoader: FeedImageLoader
-    
-    init(controller: FeedViewController?, imageLoader: FeedImageLoader) {
-        self.controller = controller
-        self.imageLoader = imageLoader
-    }
-    
-    func display(_ viewModel: FeedViewModel) {
-        controller?.feed = viewModel.feed.map {
-            let imageLoaderPresentationAdapter = ImageLoaderPresentationAdapter(imageLoader: imageLoader, model: $0)
-            let controller = FeedCellViewController(delegate: imageLoaderPresentationAdapter)
-            let presenter = FeedCellPresenter<WeakRefVirtualProxy<FeedCellViewController>, UIImage>(feedCellView: WeakRefVirtualProxy(controller), imageTransformer: UIImage.init)
-            imageLoaderPresentationAdapter.presenter = presenter
-            return controller
-        }
-    }
-}
