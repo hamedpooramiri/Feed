46be72fc385c742f23447b5799767ae43bebb775
hamed <hamedpooramiri1007@gmail.com>
2023-08-22 14:23:11 +0330

    complete implementing FeedPresenter TDD in Feed Module

--- FeedTests/Feed Presentation/FeedPresenterTests.swift
+++ FeedTests/Feed Presentation/FeedPresenterTests.swift
@@ -6,30 +6,102 @@
 //
 
 import XCTest
+import Feed
+
+struct FeedLoadingViewModel {
+    let isLoading: Bool
+}
+
+protocol FeedLoadingView {
+    func display(_ viewModel: FeedLoadingViewModel)
+}
+protocol FeedView {
+    func display(_ viewModel: FeedViewModel)
+}
+
+struct FeedViewModel {
+    let feed: [FeedItem]
+}
+
+final class FeedPresenter {
+
+    private let refreshView: FeedLoadingView
+    private let feedView: FeedView
+
+    init(refreshView: FeedLoadingView, feedView: FeedView) {
+        self.refreshView = refreshView
+        self.feedView = feedView
+    }
+
+    func didStartLoadingFeed() {
+        refreshView.display(FeedLoadingViewModel(isLoading: true))
+    }
+
+    func didFinishedLoadingFeed(with error: Error)  {
+        refreshView.display(FeedLoadingViewModel(isLoading: false))
+    }
+
+    func didFinishedLoadingFeed(with feed: [FeedItem]) {
+        feedView.display(FeedViewModel(feed: feed))
+        refreshView.display(FeedLoadingViewModel(isLoading: false))
+    }
+}
+
 
 final class FeedPresenterTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+ 
+    func test_init_doseNotSendMessageToView() {
+        let (view, _) = makeSUT()
+        XCTAssertEqual(view.messages, [])
+    }
+    
+    func test_didStartLoadingFeed_displayStartLoading() {
+        let (view, sut) = makeSUT()
+        sut.didStartLoadingFeed()
+        XCTAssertEqual(view.messages, [.display(isLoading: true)])
+    }
+    
+    func test_didFinishedLoadingFeedWithError_displayStopLoading() {
+        let (view, sut) = makeSUT()
+        let nsError = NSError(domain: "an error", code: 0)
+        sut.didFinishedLoadingFeed(with: nsError)
+        XCTAssertEqual(view.messages, [.display(isLoading: false)])
+    }
+    
+    func test_didFinishedLoadingFeed_displayFeedAndStopLoading() {
+        let (view, sut) = makeSUT()
+        sut.didFinishedLoadingFeed(with: [])
+        XCTAssertEqual(view.messages, [
+            .display(isLoading: false),
+            .display(feed: [])
+        ])
+    }
+    
+    // MARK: Helpers
+    func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (view: ViewSpy, sut: FeedPresenter){
+        let view = ViewSpy()
+        let sut = FeedPresenter(refreshView: view, feedView: view)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        trackForMemoryLeaks(view, file: file, line: line)
+        return (view, sut)
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+    class ViewSpy: FeedLoadingView, FeedView {
+        
+        enum Messages: Hashable {
+            case display(isLoading: Bool)
+            case display(feed: [FeedItem])
+        }
+        
+        var messages = Set<Messages>()
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
+        func display(_ viewModel: FeedLoadingViewModel) {
+            messages.insert(.display(isLoading: viewModel.isLoading))
+        }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+        func display(_ viewModel: FeedViewModel) {
+            messages.insert(.display(feed: viewModel.feed))
         }
     }
-
 }
