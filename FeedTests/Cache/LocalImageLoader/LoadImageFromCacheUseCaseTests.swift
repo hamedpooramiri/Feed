3f93f33f293edd22e22cf7be582bfe37f64515aa
hamed <hamedpooramiri1007@gmail.com>
2023-08-23 17:20:51 +0330

    move FeedImageStoreSpy code to its own file

--- FeedTests/Cache/LocalImageLoader/LoadImageFromCacheUseCaseTests.swift
+++ FeedTests/Cache/LocalImageLoader/LoadImageFromCacheUseCaseTests.swift
@@ -72,24 +72,4 @@ final class LoadImageFromCacheUseCaseTests: XCTestCase {
         action()
         wait(for: [exp])
     }
-
-     class FeedImageStoreSpy: FeedImageStore {
-        
-        private(set) var capturedResults: [(url: URL, completion: (FeedImageStore.Result) -> Void)] = []
-        var requestedURLs: [URL] {
-            capturedResults.map(\.url)
-        }
-        
-        func retrieveImage(for url: URL, completion: @escaping (FeedImageStore.Result) -> Void) {
-            capturedResults.append((url, completion))
-        }
-
-        func completeRetrieve(with error: Error, at index: Int = 0) {
-            capturedResults[index].completion(.failure(error))
-        }
-        
-        func completeRetrieve(with imageData: Data = Data(), at index: Int = 0) {
-            capturedResults[index].completion(.success(imageData))
-        }
-    }
 }
