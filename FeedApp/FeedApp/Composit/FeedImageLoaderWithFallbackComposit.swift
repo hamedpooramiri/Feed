69744ff889913279c09c1452e79aaaca1c1f4dc3
hamed <hamedpooramiri1007@gmail.com>
2023-08-24 23:04:05 +0330

    move FeedImageLoaderWithFallbackComposit code to its own file

--- FeedApp/FeedApp/Composit/FeedImageLoaderWithFallbackComposit.swift
+++ FeedApp/FeedApp/Composit/FeedImageLoaderWithFallbackComposit.swift
@@ -5,4 +5,38 @@
 //  Created by hamedpouramiri on 8/24/23.
 //
 
-import Foundation
+import Feed
+
+public final class FeedImageLoaderWithFallbackComposit: FeedImageLoader {
+    
+    private let primary: FeedImageLoader
+    private let fallback: FeedImageLoader
+
+    public init(primary: FeedImageLoader, fallback: FeedImageLoader) {
+        self.primary = primary
+        self.fallback = fallback
+    }
+
+    public func loadImage(with url: URL, completion: @escaping (FeedImageLoader.Result) -> Void) -> Feed.ImageLoaderTask {
+        let task = WrappedTask()
+        task.wrapped = primary.loadImage(with: url) { [weak self] result in
+            guard let self = self else { return }
+            switch result {
+            case .success:
+                completion(result)
+            case .failure:
+                task.wrapped = self.fallback.loadImage(with: url, completion: completion)
+            }
+        }
+        return task
+    }
+
+    class WrappedTask: ImageLoaderTask {
+        var wrapped: ImageLoaderTask?
+        
+        func cancel() {
+            wrapped?.cancel()
+        }
+    }
+
+}
